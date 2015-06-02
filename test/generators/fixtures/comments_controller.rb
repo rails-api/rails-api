1fbfff967af94a109b1178a957d0db2f6603947a
Paul Tashman <paul.tashman@careerbuilder.com>
2015-06-02 13:13:35 -0400

    Adds scope generator with tests

---
+++ test/generators/fixtures/comments_controller.rb
@@ -0,0 +1,59 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  # GET /comments
+  # GET /comments.json
+  def index
+    @comments = Comment.all
+
+    render json: @comments
+  end
+
+  # GET /comments/1
+  # GET /comments/1.json
+  def show
+    render json: @comment
+  end
+
+  # POST /comments
+  # POST /comments.json
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      head :no_content
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params[:comment]
+    end
+end
---
+++ test/generators/scope_generator_test.rb
@@ -0,0 +1,32 @@
+require 'generators/generators_test_helper'
+require 'rails-api/generators/rails/scope/scope_generator'
+require 'pry'
+class ScopeGeneratorTest < Rails::Generators::TestCase
+  include GeneratorsTestHelper
+
+  tests ScopeGenerator
+  arguments %w(comment best)
+
+  setup :copy_routes
+  setup :copy_controller
+  setup :copy_model
+  
+  def test_scope_route_and_methods_are_added
+    binding.pry
+    run_generator
+
+    assert_file "config/routes.rb" do |content|
+      assert_match(/get 'best', on: :collection/, content)
+    end
+
+    assert_file "app/controllers/comments_controller.rb" do |content|
+      assert_match(/def best/, content)
+      assert_match(/@comments = Comment.best/, content)
+      assert_match(/render json: @comments/, content)
+    end
+
+    assert_file "app/models/comment.rb" do |content|
+      assert_match(/scope :best, -> { all }/, content)
+    end
+  end
+end
